0590d0e25964ca657b660f06e8287836a02ce84b
Arnau <arnau@ersilia.io>
2025-07-01 13:55:25 +0200

    building model

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,8 +2,13 @@
 import os
 import csv
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+from urllib.request import urlretrieve
+from rdkit.Chem import MolFromSmiles
+from chemprop import featurizers, nn
+from chemprop.data import BatchMolGraph
+from chemprop.nn import RegressionFFN
+from chemprop.models import MPNN
+import torch
 
 # parse arguments
 input_file = sys.argv[1]
@@ -12,9 +17,13 @@ output_file = sys.argv[2]
 # current file directory
 root = os.path.dirname(os.path.abspath(__file__))
 
-# my model
-def my_model(smiles_list):
-    return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
+# for extra info, check this issue https://github.com/JacksonBurns/chemeleon/issues/16
+
+@torch.no_grad
+def chemeleon_fingerprint(smiles: list[str], model: MPNN, featurizer: featurizers.SimpleMoleculeMolGraphFeaturizer):  # i.e. get_embeddings
+    bmg = BatchMolGraph([featurizer(MolFromSmiles(s)) for s in smiles])
+    bmg.to(device=model.device)
+    return model.fingerprint(bmg).numpy(force=True)
 
 
 # read SMILES from .csv file, assuming one column with header
@@ -23,8 +32,17 @@ with open(input_file, "r") as f:
     next(reader)  # skip header
     smiles_list = [r[0] for r in reader]
 
-# run model
-outputs = my_model(smiles_list)
+# Load model for evaluation
+featurizer = featurizers.SimpleMoleculeMolGraphFeaturizer()
+agg = nn.MeanAggregation()
+chemeleon_mp = torch.load("../checkpoints/chemeleon_mp.pt", weights_only=True)
+mp = nn.BondMessagePassing(**chemeleon_mp['hyper_parameters'])
+mp.load_state_dict(chemeleon_mp['state_dict'])
+model = MPNN(message_passing=mp, agg=agg, predictor=RegressionFFN())
+model.eval()
+
+# Run model
+outputs = chemeleon_fingerprint(smiles_list, model, featurizer)
 
 #check input and output have the same lenght
 input_len = len(smiles_list)
@@ -34,6 +52,7 @@ assert input_len == output_len
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"])  # header
+    header = [f"dim_{str(i).zfill(4)}" for i in range(2048)]
+    writer.writerow(header)
     for o in outputs:
-        writer.writerow([o])
+        writer.writerow(o)
